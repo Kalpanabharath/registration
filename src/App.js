b0762f10875ac9a48010cb8b5285927a3aecda7c
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-11-12 16:29:56 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Registration from "./Registration";
 import CreateUser from "./CreateUser";
