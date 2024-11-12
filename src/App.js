c5336899124a44ad8350db40879429a0a12e3c1c
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-11-12 16:07:14 +0530

    .

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/registration" element={<Registration />}></Route>
+        <Route path="/registration/" element={<Registration />}></Route>
         <Route path="/user/create" element={<CreateUser />}></Route>
         <Route path="/user/edit/:userid" element={<EditUser />}></Route>
         <Route path="/user/view/:userid" element={<ViewUser />}></Route>
