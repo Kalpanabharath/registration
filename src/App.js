dd6ad917a6ca6a6a5b7fb227e741c0177bbac91a
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-11-12 22:01:52 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; // No need for Switch in v6
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Registration from "./Registration";
 import CreateUser from "./CreateUser";
@@ -8,12 +8,18 @@ import ViewUser from "./ViewUser";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Registration />} />
-      <Route path="/user/create" element={<CreateUser />} />
-      <Route path="/user/edit/:userid" element={<EditUser />} />
-      <Route path="/user/view/:userid" element={<ViewUser />} />
-    </Routes>
+    <div>
+      <Link to="/user/create">
+        <p>Go to Create User Page</p>
+      </Link>
+
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/user/create" element={<CreateUser />} />
+        <Route path="/user/edit/:userid" element={<EditUser />} />
+        <Route path="/user/view/:userid" element={<ViewUser />} />
+      </Routes>
+    </div>
   );
 }
 
