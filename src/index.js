99bdf381fffb550bd25952f10cd447b8c45242eb
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-11-12 15:57:24 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // import HashRouter
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/registration">
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
